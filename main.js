57b18d659daae327b1960c37b40d02b71b411740
nnuoyos <youngadult3104@gmail.com>
2022-08-27 17:39:34 +0900

    카카오맵 테스트 03

--- main.js
+++ main.js
@@ -132,9 +132,10 @@ function onClickSelect(e) {
 });
 
 /* kakao map API */
-const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-const options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(37.466390317686354, 127.03279034552385), //지도의 중심좌표.
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
 	level: 3 //지도의 레벨(확대, 축소 정도)
 };
-const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
