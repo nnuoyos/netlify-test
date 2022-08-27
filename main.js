16351956cf2d3cde8a3ba9a2cbf3716d09d5a7f3
nnuoyos <youngadult3104@gmail.com>
2022-08-27 18:31:31 +0900

    카카오맵 확인 테스트 04

--- main.js
+++ main.js
@@ -134,8 +134,19 @@ function onClickSelect(e) {
 /* kakao map API */
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	center: new kakao.maps.LatLng(37.466390317686354, 127.03279034552385), //지도의 중심좌표.
 	level: 3 //지도의 레벨(확대, 축소 정도)
 };
-
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+// 마커가 표시될 위치입니다
+var markerPosition  = new kakao.maps.LatLng(37.466390317686354, 127.03279034552385);
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);  
